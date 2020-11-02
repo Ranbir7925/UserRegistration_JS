922a3c64ce9b356cd5c5d33d38445a2967b45300
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-02 18:44:28 +0530

    [Add]:Code to validate email id

--- UserRegistration.js
+++ UserRegistration.js
@@ -3,10 +3,12 @@ console.log("Name should start with Capital letter and should have minimum 3 cha
 
 const prompt = require('prompt-sync')()
 const namePattern = new RegExp("^[A-Z][A-Za-z]{2,}$");
+const emailPattern = new RegExp("^[0-9a-zA-Z]+[.]*[0-9a-zA-z]*[@][a-zA-Z]+([.][a-zA-Z]+){1,3}$");
 
-function checkValidation(name, namePattern) {
+
+function checkValidation(name, Pattern) {
     let flag
-    if (namePattern.test(name)) {
+    if (Pattern.test(name)) {
         console.log(" Saved");
         flag = false
     }
@@ -26,6 +28,13 @@ while (true) {
 while (true) {
     let lastName = prompt("Enter your last name: ")
     let check = checkValidation(lastName, namePattern)
+    if (check == false)
+        break;
+}
+
+while (true) {
+    let email = prompt("Enter your email id: ")
+    let check = checkValidation(email, emailPattern)
     if (check == false)
         break;
 }
