cdf87522b612097a933fea35c74afd8b9af99cf7
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-02 19:18:09 +0530

    [Style]:Code formatting done

--- UserRegistration.js
+++ UserRegistration.js
@@ -2,6 +2,7 @@ console.log("Welcome to User Registration ..");
 console.log("Name should start with Capital letter and should have minimum 3 characters.");
 
 const prompt = require('prompt-sync')()
+
 const namePattern = new RegExp("^[A-Z][A-Za-z]{2,}$");
 const emailPattern = new RegExp("^[0-9a-zA-Z]+[.]*[0-9a-zA-z]*[@][a-zA-Z]+([.][a-zA-Z]+){1,3}$");
 const phoneNumberPattern = new RegExp("^[0-9]{2}[ ][0-9]{10}$");
@@ -17,36 +18,36 @@ function checkValidation(name, Pattern) {
         console.log("Invalid .. Please enter agian ");
         flag = true
     }
-    return flag 
+    return flag
 }
 
-// while (true) {
-//     let firstName = prompt("Enter your first name: ")
-//     let check = checkValidation(firstName, namePattern)
-//     if (check == false)
-//         break;
-// }
+while (true) {
+    let firstName = prompt("Enter your first name: ")
+    let check = checkValidation(firstName, namePattern)
+    if (check == false)
+        break;
+}
 
-// while (true) {
-//     let lastName = prompt("Enter your last name: ")
-//     let check = checkValidation(lastName, namePattern)
-//     if (check == false)
-//         break;
-// }
+while (true) {
+    let lastName = prompt("Enter your last name: ")
+    let check = checkValidation(lastName, namePattern)
+    if (check == false)
+        break;
+}
 
-// while (true) {
-//     let email = prompt("Enter your email id: ")
-//     let check = checkValidation(email, emailPattern)
-//     if (check == false)
-//         break;
-// }
+while (true) {
+    let email = prompt("Enter your email id: ")
+    let check = checkValidation(email, emailPattern)
+    if (check == false)
+        break;
+}
 
-// while (true) {
-//     let phoneNumber = prompt("Enter your PhoneNumber: ")
-//     let check = checkValidation(phoneNumber, phoneNumberPattern)
-//     if (check == false)
-//         break;
-// }
+while (true) {
+    let phoneNumber = prompt("Enter your PhoneNumber: ")
+    let check = checkValidation(phoneNumber, phoneNumberPattern)
+    if (check == false)
+        break;
+}
 
 while (true) {
     let password = prompt("Enter your Password: ")
